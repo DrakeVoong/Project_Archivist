80801da87ad3fd4347f5857c9421bbd15b3530c9
Drake Voong <drakevoong404@gmail.com>
2026-05-25 19:04:33 -0700

    refactor: remove deprecated files

--- archivist/nodes/webui/send_message.py
+++
@@ -1,7 +0,0 @@
-from typing import Generator
-
-from archivist.nodes.node_handler import node
-
-@node(inputs=["response"], send_outputs=["response"])
-def send_message(response: Generator) -> tuple[Generator]:
-    return response
